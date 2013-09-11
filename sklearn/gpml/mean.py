1b7a8432c2d10bf52649861564769774b26e83e2
Dejan Petelin <dejan5elin@gmail.com>
2013-09-11 14:25:22 +0200

    Implemented simple and composite mean functions.

--- sklearn/gpml/mean.py
+++ sklearn/gpml/mean.py
@@ -1,9 +1,34 @@
+"""
+Mean functions to be use by Gaussian process functions. There are two
+different kinds of mean functions:: simple and composite::
+
+simple mean functions::
+
+  zero      - zero mean function
+  one       - one mean function
+  const     - constant mean function
+  linear    - linear mean function
+  
+composite covariance functions::
+
+  scale     - scaled version of a mean function
+  pow       - power of a mean function
+  prod      - products of mean functions
+  sum       - sums of mean functions
+  mask      - mask some dimensions of the data
+"""
+# Author: Dejan Petelin <http://www.linkedin.com/in/dejanpetelin>
+# Python implementation of the GPML MATLAB Toolbox, version 3.2
+# License: see copyright
+
 import numpy
 
-# Zero mean function. The mean function does not have any parameters.
-#
-# m(x) = 0
 def zero(hyp, x=None, hi=None):
+  """
+  Zero mean function. The mean function does not have any parameters.
+  
+  m(x) = 0
+  """
   if x is None:
     return '0'
   
@@ -11,31 +36,282 @@ def zero(hyp, x=None, hi=None):
   return numpy.zeros((numpy.size(x,0),1))
 
 
+def one(hyp, x=None, hi=None):
+  """
+  One mean function. The mean function does not have any parameters.
+  
+  m(x) = 1
+  """
+  if x is None:
+    return '0'
+  
+  if hi is None:
+    # mean
+    return numpy.ones((numpy.size(x,0),1))
+  else:
+    # derivative
+    return numpy.zeros((numpy.size(x,0),1))
 
-def mask(mean, hyp, x=None, hi=None):
-  raise NotImplementedError('')
 
-def pow(mean, hyp, x=None, hi=None):
-  raise NotImplementedError('')
+def const(hyp, x=None, hi=None):
+  """
+  Constant mean function. The mean function is parameterized as::
+  
+  m(x) = c
+  
+  The hyperparameter is::
+  
+  hyp = [ c ]
+  """
+  if x is None:
+    return '1'
 
-def prod(mean, hyp, x=None, hi=None):
-  raise NotImplementedError('')
+  if numpy.size(hyp) != 1:
+    raise AttributeError('Exactly one hyperparameter needed.')
 
-def scale(mean, hyp, x=None, hi=None):
-  raise NotImplementedError('')
+  c = numpy.reshape(hyp, (-1,))[0]
+  if hi is None:
+    # mean
+    return c*numpy.ones((numpy.size(x,0),1))
+  else:
+    # derivative
+    if hi == 0:
+      return numpy.ones((numpy.size(x,0),1))
+    else:
+      return numpy.zeros((numpy.size(x,0),1))
 
-def sum(mean, hyp, x=None, hi=None):
-  raise NotImplementedError('')
 
-# Evaluates mean functions
+def linear(hyp, x=None, hi=None):
+  """
+  Linear mean function. The mean function is parameterized as::
+  
+  m(x) = sum_i a_i * x_i
+  
+  The hyperparameters are::
+  
+  hyp = [ a_1
+          a_2
+          ...
+          a_D]
+  """
+  if x is None:
+    return 'D'
+
+  n, D = numpy.shape(x)
+  if numpy.shape(hyp) != (D,1):
+    raise AttributeError('Exactly D hyperparameters needed.')
+  
+  a = hyp
+  if hi is None:
+    # mean
+    return numpy.dot(x, a)
+  else:
+    # derivative
+    if hi < D:
+      return x[:,hi:hi+1]
+    else:
+      return numpy.zeros((n,1))
+
+
+def scale(meanf, hyp, x=None, hi=None):
+  """
+  Compose a mean function as a scaled version of another one::
+  
+  m(x) = a * m_0(x)
+  
+  The hyperparameter is::
+  
+  hyp = [ a ]
+  
+  This function doesn't actually compute very much on its own, it merely does
+  some bookkeeping, and calls other mean function to do the actual work.
+  """
+  if x is None:
+    return feval(meanf, '+1')
+
+  n, D = numpy.shape(x)
+  a = numpy.reshape(hyp, (-1,))[0]
+  if hi is None:
+    # mean
+    return a*feval(meanf,hyp[1:],x)
+  else:
+    # derivative
+    if hi == 0:
+      return feval(meanf,hyp[1:],x)
+    else:
+      return a*feval(meanf,hyp[1:],x,hi-1)
+
+
+def pow(meanf, hyp, x=None, hi=None):
+  """
+  Compose a mean function as the power of another mean function::
+  
+  m(x) = m_0(x) ^ d
+  
+  The degree d has to be a strictly positive integer. It is defined in the 
+  mean function as it is not to be optimized.
+  
+  This function doesn't actually compute very much on its own, it merely does
+  some bookkeeping, and calls other mean function to do the actual work.
+  """
+  d = meanf[0]
+  if not isinstance(d, (int, long, float)):
+    raise AttributeError('First element of pow mean function must be a number.')
+  d = max(numpy.abs(int(d)),1)
+  meanf = meanf[1]
+
+  if x is None:
+    return feval(meanf)
+
+  n, D = numpy.shape(x)
+  if hi is None:
+    # mean
+    return numpy.power(feval(meanf,hyp,x), d)
+  else:
+    # derivative
+    return (d*numpy.power(feval(meanf, hyp, x), (d-1))) * feval(meanf, hyp, x, hi);
+
+
+def prod(meanf, hyp, x=None, hi=None):
+  """
+  Compose a mean function as the product of other mean functions::
+  
+  m(x) = prod_i m_i(x)
+  
+  This function doesn't actually compute very much on its own, it merely does
+  some bookkeeping, and calls other mean function to do the actual work.
+  """
+  j = []
+  for i in range(len(meanf)):
+    f = meanf[i]
+    j.append(feval(f))
+  
+  if x is None:
+    t = j[0]
+    for i in range(1, len(meanf)):
+      t = t + '+' + j[i]
+    return t
+
+  n, D = numpy.shape(x)
+  
+  # v vector indicates to which mean parameters belong
+  v = numpy.array([])
+  for i in range(len(meanf)):
+    v = numpy.append(v, numpy.tile(i, (eval(j[i]),)))
+
+  r = numpy.ones((n,1))
+  if hi is None:
+    # compute mean by iteration over factor functions
+    for i in range(len(meanf)):
+      f = meanf[i]
+      # accumulate means
+      r = r * feval(f, hyp[v==i], x)
+    return r
+  else:
+    # derivative
+    if hi < numpy.size(v):
+      i = v[hi]
+      hj = numpy.sum(v[0:hi]==i)
+      for j in range(len(meanf)):
+        f = meanf[j]
+        if j == i:
+          # multiply derivative
+          r = r * feval(f, hyp[v==j], x, hj)
+        else:
+          # multiply mean
+          r = r * feval(f, hyp[v==j], x)
+      return r
+    else:
+      return numpy.zeros((n,1))
+
+
+def sum(meanf, hyp, x=None, hi=None):
+  """
+  Compose a mean function as the sum of other mean functions::
+  
+  m(x) = sum_i m_i(x)
+  
+  This function doesn't actually compute very much on its own, it merely does
+  some bookkeeping, and calls other mean function to do the actual work.
+  """
+  j = []
+  for i in range(len(meanf)):
+    f = meanf[i]
+    j.append(feval(f))
+  
+  if x is None:
+    t = j[0]
+    for i in range(1, len(meanf)):
+      t = t + '+' + j[i]
+    return t
+
+  n, D = numpy.shape(x)
+  
+  # v vector indicates to which mean parameters belong
+  v = numpy.array([])
+  for i in range(len(meanf)):
+    v = numpy.append(v, numpy.tile(i, (eval(j[i]),)))
+
+  if hi is None:
+    r = numpy.zeros((n,1))
+    # compute mean by iteration over summand functions
+    for i in range(len(meanf)):
+      f = meanf[i]
+      # accumulate means
+      r = r + feval(f, hyp[v==i], x)
+    return r
+  else:
+    # derivative
+    if hi < numpy.size(v):
+      i = v[hi]
+      hj = numpy.sum(v[0:hi]==i)
+      f = meanf[j]
+      return feval(f, hyp[v==i], x, hj)
+    else:
+      return numpy.zeros((n,1))
+
+
+def mask(meanf, hyp, x=None, hi=None):
+  """
+  Apply a mean function to a subset of the dimensions only. The subset can
+  either be specified by a 0/1 mask, by a boolean mask or by an index set.
+  
+  This function doesn't actually compute very much on its own, it merely does
+  some bookkeeping, and calls other mean function to do the actual work.
+  """
+  mask = numpy.fix(meanf[0])
+  meanf = meanf[1]
+  nh = feval(meanf)
+
+  if numpy.max(mask) < 2 and numpy.size(mask) > 1:
+    mask = numpy.nonzero(mask)[0]
+  D = len(mask)
+  if x is None:
+    return str(eval(nh))
+
+  if D > numpy.size(x,1):
+    raise AttributeError('Size of masked data does not match the dimension of data.')
+  if eval(nh) != numpy.size(hyp):
+    raise AttributeError('Number of hyperparameters does not match size of masked data.')
+
+  if hi is None:
+    # mean
+    return feval(meanf, hyp, x[:,mask])
+  else:
+    # derivative
+    return feval(meanf, hyp, x[:,mask], hi)
+
+
 def feval(fun, hyp=None, x=None, hi=None):
+  """
+  Evaluates mean functions.
+  """
   if not isinstance(fun, tuple):
     fun = (fun,)
 
   f = fun[0]
-  #if f.__module__ == 'mean':
   if f.__module__ == 'sklearn.gpml.mean':
-    if len(fun) > 1 and (f == mean.mask or f == mean.pow or f == mean.prod or f == mean.scale or f == mean.sum):
+    if len(fun) > 1 and (f == mask or f == pow or f == prod or f == scale or f == sum):
       return f(fun[1], hyp, x, hi)
     else:
       return f(hyp, x, hi)
